bab76f4d106d90ba3b6e01ef94ebb5b77bf8b6eb
MorrisPryce <96196461+MorrisPryce@users.noreply.github.com>
2022-01-17 20:09:36 +0000

    added pre02 data-types

---
+++ data-types/DataTypes.java
@@ -0,0 +1,21 @@
+class DataTypes {
+public static void main(String[] args){
+int number;
+int value = 30;
+int sum;
+
+number = 20;
+
+sum = number + value;
+number = number - 1;
+
+System.out.println("Data");
+
+System.out.println(number);
+
+System.out.println("The Sum of " +number+" + " +value+ " = " +sum);
+System.out.println("number: "+number);
+System.out.println("value:"+value);
+System.out.println("sum:"+sum);
+}
+}
