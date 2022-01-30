f7d7120b64bea9d7d49a502cc3311f2d90013104
MorrisPryce <96196461+MorrisPryce@users.noreply.github.com>
2022-01-30 20:04:26 +0000

    Added pre04

---
+++ pre04/Conditional.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Conditional{
+  
+public static void main (String [] args){
+int a;
+ a = 5;
+
+ if (a > 10){
+   System.out.println("A is bigger");
+   System.out.println("if not in");
+ } else{
+System.out.println("It's < or =");
+System.out.println("if not in");
+ }
+System.out.println("positive single digit number.");
+}
+}
---
+++ pre04/NewConditional.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class NewConditional{
+
+public static void main(String[] args){
+ int x; 
+x = 7;
+if (x > 0 && x < 10) {
+    //if (x < 10) {
+        System.out.println("positive single digit number.");
+    //}
+}
+System.out.println("Negative single digit number");
+
+}
+
+}
+
+
+
+
+
+
+  
