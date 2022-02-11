085969fdf954d44ce4a4c5cc3513c144ff27ee8b
MorrisPryce <96196461+MorrisPryce@users.noreply.github.com>
2022-02-11 18:00:02 +0000

    Changed lesson to pre02

---
+++ pre02/Date.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Date{
+
+public static void main(String[] args){
+int date = 24;
+int year = 2022;
+printAmerica("Monday" , "January" , date , year);
+}
+
+public static void printAmerica(String day, String month, int date, int year){
+System.out.println(day);
+System.out.println(month);
+System.out.println(date);
+System.out.println(year);
+System.out.println(day + ", " + month + " " + date + ", " + year);
+
+}
+}
