b229afef5b63db054b6ddd6de75cebbefb2d0390
MorrisPryce <96196461+MorrisPryce@users.noreply.github.com>
2022-02-13 21:17:21 +0000

    changes to pre02

--- pre02/Date.java
+++ pre02/Date.java
@@ -11,10 +11,10 @@ printAmerica("Monday" , "January" , date , year);
 }
 
 public static void printAmerica(String day, String month, int date, int year){
-System.out.println(day);
-System.out.println(month);
-System.out.println(date);
-System.out.println(year);
+/*System.out.println(day);
+System.out.println( month);
+System.out.println( date);
+System.out.println( year);*/
 System.out.println(day + ", " + month + " " + date + ", " + year);
 
 }
---
+++ pre02/Time.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Time{
+
+public static void main(String[] args){
+int hour = 15;
+int minute = 20;
+int second = 60;
+//System.out.println(hour);
+currentTime(hour, minute, second);
+}
+public static void currentTime(int hour, int minute, int second){
+System.out.print(hour);
+System.out.print(" : ");
+System.out.print(minute);
+System.out.print(" : ");
+System.out.println(second);
+System.out.println("Number of seconds since midnight:" +(second + (minute*60)));
+}  
+
+}
